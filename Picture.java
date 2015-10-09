df5c0a74311af7db8cd3944d71aa4b0ecb32358c
Manuel <manubp9@gmail.com>
2015-10-09 11:04:25 +0200

    suprimido circle 2 y cambiado a amarillo circle

--- Picture.java
+++ Picture.java
@@ -15,7 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
-    private Circle sun2;
+    
 
     /**
      * Constructor for objects of class Picture
@@ -50,18 +50,13 @@ public class Picture
         roof.makeVisible();
 
         sun = new Circle();
-        sun.changeColor("blue");
+        sun.changeColor("yellow");
         sun.moveHorizontal(100);
         sun.moveVertical(-40);
         sun.changeSize(80);
         sun.makeVisible();
         
-        sun = new Circle();
-        sun.changeColor("green");
-        sun.moveHorizontal(-100);
-        sun.moveVertical(-50);
-        sun.changeSize(80);
-        sun.makeVisible();
+    
         
     }
 
